ce968da80c05c446307408fda923569661d32508
yvsaurabh <yvsaurabh0707@gmail.com>
2026-05-29 16:47:34 +0530

    updating cloudinary for storing the images instead of local storage of multer

--- routes/listing.js
+++ routes/listing.js
@@ -1,24 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-
-const uploadDir = path.join(__dirname, "../public/uploads");
-fs.mkdirSync(uploadDir, { recursive: true });
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueName = `${Date.now()}${path.extname(file.originalname)}`;
-        cb(null, uniqueName);
-    },
-});
+const { storage } = require("../cloudConfig.js");
 
 const upload = multer({ storage });
 
@@ -27,7 +13,7 @@ const setUploadedListingImage = (req, res, next) => {
         req.body.listing = req.body.listing || {};
         req.body.listing.image = {
             filename: req.file.filename,
-            url: `/uploads/${req.file.filename}`,
+            url: req.file.path,
         };
     }
 
@@ -38,7 +24,7 @@ router.route("/")
      .get(wrapAsync(listingController.index))
      .post(
         isLoggedIn,
-        upload.single("listing[imageFile]"),
+        upload.single("listing[image]"),
         setUploadedListingImage,
         validateListing,
         wrapAsync(listingController.createListing)
